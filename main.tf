ffb1a990a3f8f4b314aa9d9b41163b1a858ab74c
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-06-08 18:22:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ terraform {
 
 
 resource "aws_instance" "EC2-Instance" {
-  ami           = "ami-0487b1fe60c1fd1a2"
-  instance_type = "t2.micro"
-  security_groups= [aws_security_group.security_grp.name]
+  ami             = "ami-0487b1fe60c1fd1a2"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.security_grp.name]
   tags= {
     Name = "ec2-instance"
   }
@@ -34,15 +34,12 @@ resource "aws_instance" "EC2-Instance" {
 resource "aws_security_group" "security_grp" {
   name        = "EC2-security-group"
   description = "security group for EC2 instance"
-
-
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags= {
     Name = "EC2-security-group"
   }
