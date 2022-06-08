46fb917edb46a2fd4ceb588f41ea7f8350cdb1aa
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-06-08 18:21:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,18 +22,17 @@ terraform {
 
 
 resource "aws_instance" "EC2-Instance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
+  ami           = "ami-0487b1fe60c1fd1a2"
+  instance_type = "t2.micro"
+  security_groups= [aws_security_group.security_grp.name]
   tags= {
-    Name = var.tag_name
+    Name = "ec2-instance"
   }
 }
 
 #Create security group with firewall rules
 resource "aws_security_group" "security_grp" {
-  name        = var.security_group
+  name        = "EC2-security-group"
   description = "security group for EC2 instance"
 
 
@@ -45,33 +44,6 @@ resource "aws_security_group" "security_grp" {
   }
 
   tags= {
-    Name = var.security_group
+    Name = "EC2-security-group"
   }
-}
-
-#------------------------------Variable.tf--------------------------------
-
-variable "aws_region" {
-       description = "The AWS region to create things in."
-       default     = "us-west-1"
-}
-
-
-variable "instance_type" {
-    description = "instance type for ec2"
-    default     =  "t2.micro"
-}
-
-variable "security_group" {
-    description = "Name of security group"
-    default     = "EC2-security-group"
-}
-
-variable "tag_name" {
-    description = "Tag Name of for Ec2 instance"
-    default     = "ec2-instance"
-}
-variable "ami_id" {
-    description = "AMI for Ubuntu Ec2 instance"
-    default     = "ami-0487b1fe60c1fd1a2"
 }
