9c6d2d2e27b5c5248a451d1d75f4c52c340f05b4
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-06-08 18:09:57 +0530

    Update and rename yog to main.tf

---
+++ main.tf
@@ -0,0 +1,55 @@
+
+resource "aws_instance" "EC2-Instance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "security_grp" {
+  name        = var.security_group
+  description = "security group for EC2 instance"
+
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+#------------------------------Variable.tf--------------------------------
+
+variable "aws_region" {
+       description = "The AWS region to create things in."
+       default     = "eu-west-1"
+}
+
+
+variable "instance_type" {
+    description = "instance type for ec2"
+    default     =  "t2.micro"
+}
+
+variable "security_group" {
+    description = "Name of security group"
+    default     = "EC2-security-group"
+}
+
+variable "tag_name" {
+    description = "Tag Name of for Ec2 instance"
+    default     = "ec2-instance"
+}
+variable "ami_id" {
+    description = "AMI for Ubuntu Ec2 instance"
+    default     = "ami-0487b1fe60c1fd1a2"
+}
