f710135c90ad0ad8ddf49aced3b6fa2a9cebebc5
Yogender <104613019+YogenderJ@users.noreply.github.com>
2022-06-08 18:29:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,11 +20,21 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-west-1"
+}
 
 resource "aws_instance" "EC2-Instance" {
-  ami             = "ami-0487b1fe60c1fd1a2"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.security_grp.name]
+  ami                     = "ami-0487b1fe60c1fd1a2"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.security_grp.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
   tags= {
     Name = "ec2-instance"
   }
@@ -35,12 +45,15 @@ resource "aws_security_group" "security_grp" {
   name        = "EC2-security-group"
   description = "security group for EC2 instance"
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags= {
     Name = "EC2-security-group"
   }
+}
+output "web-address" {
+  value = "${aws_instance.EC2-Instance.public_dns}:80"
 }
